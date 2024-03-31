11c650a66b2fc072d125ea816fc15f53510a62dd
JiratchayaPremsil <156444689+JiratchayaPremsil@users.noreply.github.com>
2024-03-31 15:00:58 +0700

    Update Recipe.java

--- src/gameLogic/Recipe.java
+++ src/gameLogic/Recipe.java
@@ -7,18 +7,23 @@ public class Recipe {
     private final String foodName;
     private final int timeAmount;
     private boolean isLock;
+    private final String[] words;
 
-    public Recipe(ArrayList<Ratio> recipe, String foodname, int timeAmount, boolean isLock) {
+    public Recipe(ArrayList<Ratio> recipe, String foodname, int timeAmount, boolean isLock, String[] words) {
         Recipe = recipe;
         this.foodName = foodname;
         this.timeAmount = Math.max(5, timeAmount);
         this.isLock = isLock;
+        this.words = words;
     }
 
     public ArrayList<Ratio> getRecipe() {
         return Recipe;
     }
 
+    public String[] getWords() {
+        return words;
+    }
 
     public String getFoodName() {
         return foodName;
@@ -38,3 +43,4 @@ public class Recipe {
         isLock = lock;
     }
 }
+
