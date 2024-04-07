53c5eba45710627f676dba1115898a75bc559798
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-04-07 18:40:20 +0700

    + Main menu complete

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,134 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import pane.RootPane;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
+    private static final Font FONT = Font.font("", FontWeight.BOLD, 42);
+    private VBox menuBox;
+    private int currentItem = 0;
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(1440, 1080);
+
+        // Load and set the background image
+        Image bgImage = new Image(Objects.requireNonNull(getClass().getResource("/kitchen.png")).toExternalForm());
+        BackgroundImage backgroundImage = new BackgroundImage(bgImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        root.setBackground(new Background(backgroundImage));
+
+        MenuItem itemExit = new MenuItem("EXIT");
+        itemExit.setOnActivate(() -> System.exit(0));
+
+        menuBox = new VBox(10,
+                new MenuItem("START"),
+                new MenuItem("GUIDE"),
+                itemExit);
+        menuBox.setAlignment(Pos.TOP_CENTER);
+        menuBox.setTranslateX(220);
+        menuBox.setTranslateY(185);
+
+        getMenuItem(0).setActive(true);
+
+        root.getChildren().addAll(menuBox);
+        return root;
+    }
+
+
+    private MenuItem getMenuItem(int index){
+        return (MenuItem)menuBox.getChildren().get(index);
+    }
+
+    private static class MenuItem extends HBox {
+        private Star star1 = new Star();
+        private Star star2 = new Star();
+
+        private Text text;
+        private Runnable script;
+
+        public MenuItem(String name){
+            super(15);
+            setAlignment(Pos.CENTER);
+
+            text = new Text(name);
+            text.setFont(FONT);
+            text.setEffect(new GaussianBlur(2));
+
+            getChildren().addAll(star1 , text , star2 );
+            setActive(false);
+            setOnActivate(() -> System.out.println(name + " activated"));
+        }
+
+        private static class Star extends Parent {
+            public Star(){
+                ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResource("/star1.png")).toExternalForm()));
+
+                imageView.setFitWidth(25);
+                imageView.setFitHeight(25);
+                imageView.setEffect(new GaussianBlur(2));
+
+                getChildren().add(imageView);
+
+            }
+        }
+
+        public void setActive(boolean b){
+            star1.setVisible(b);
+            star2.setVisible(b);
+            text.setFill(b ? Color.WHITE : Color.GRAY);
+        }
+
+        public void setOnActivate(Runnable r){
+            script = r;
+        }
+
+        public void activate(){
+            if(script != null)
+                script.run();
+        }
+    }
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(RootPane.getRootPane(), 1440, 1080);
+        Scene scene = new Scene(createContent(), 1440, 1080);
+        scene.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.UP){
+                if(currentItem > 0){
+                    getMenuItem(currentItem).setActive(false);
+                    getMenuItem(--currentItem).setActive(true);
+                }
+            }
+            if(event.getCode() == KeyCode.DOWN){
+                if(currentItem < menuBox.getChildren().size() - 1){
+                    getMenuItem(currentItem).setActive(false);
+                    getMenuItem(++currentItem).setActive(true);
+                }
+            }
+
+            if(event.getCode() == KeyCode.ENTER){
+                getMenuItem(currentItem).activate();
+            }
+        });
 
         stage.setScene(scene);
 
--- src/pane/RootPane.java
+++ src/pane/RootPane.java
@@ -20,19 +20,8 @@ public class RootPane extends VBox {
     private static RootPane instance;
 
     private RootPane() {
-        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-        this.setAlignment(Pos.TOP_CENTER);
-        this.setSpacing(16);
-        this.setPadding(new Insets(32, 0, 32, 0));
-        Text text = new Text("Cooking Game");
-        text.setFill(Color.DARKCYAN);
-        text.setFont(Font.font("Verdana", FontWeight.BOLD, 32));
-        this.getChildren().add(text);
-
-        Image bgImage = new Image(Objects.requireNonNull(getClass().getResource("/resource/kitchen.png")).toExternalForm());
+        Image bgImage = new Image(Objects.requireNonNull(getClass().getResource("/kitchen.png")).toExternalForm());
         ImageView imageView = new ImageView(bgImage);
-        imageView.setFitWidth(1080);
-        imageView.setPreserveRatio(true);
         this.getChildren().add(imageView);
     }
 
