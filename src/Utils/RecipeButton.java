fc4933070d573e6f82b14e7ea7a68bfb80c1e6ca
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-04-29 17:59:20 +0700

    + button idle animation

--- src/Utils/RecipeButton.java
+++ src/Utils/RecipeButton.java
@@ -1,8 +1,6 @@
 package Utils;
 
-import javafx.animation.RotateTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.effect.Glow;
@@ -33,12 +31,22 @@ public class RecipeButton {
         rotateTransition.setCycleCount(Timeline.INDEFINITE);
         rotateTransition.setAutoReverse(true);
 
+        Timeline pulseAnimation = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(glow.levelProperty(), 0.0)),
+                new KeyFrame(Duration.seconds(1.5), new KeyValue(glow.levelProperty(), 0.6)),
+                new KeyFrame(Duration.seconds(2), new KeyValue(glow.levelProperty(), 0.0))
+        );
+        pulseAnimation.setCycleCount(Timeline.INDEFINITE);
+
+        pulseAnimation.play();
+
         button.setOnMouseEntered(event -> {
             // Start scale up animation
             scaleTransitionEnter.play();
             // Increase glow effect
             glow.setLevel(0.5);
             rotateTransition.play();
+            pulseAnimation.stop();
         });
 
         button.setOnMouseExited(event -> {
@@ -47,6 +55,7 @@ public class RecipeButton {
             // Reset glow effect
             glow.setLevel(0.0);
             rotateTransition.stop();
+            pulseAnimation.play();
         });
 
         button.setOnMousePressed(new EventHandler<MouseEvent>() {
